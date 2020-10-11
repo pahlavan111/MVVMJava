6a93502338670543171cf105f4372981a9c1e52e
behrooz pahlavan <pahlavan111@gmail.com>
2020-10-12 00:35:52 +0330

    add dependencies add entity class

---
+++ app/src/main/java/com/bp/mvvmjava/model/Note.java
@@ -0,0 +1,41 @@
+package com.bp.mvvmjava.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String description;
+    private int priority;
+
+    public Note(String name, String description, int priority) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
