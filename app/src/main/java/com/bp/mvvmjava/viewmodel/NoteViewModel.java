ebad1ad40712dca6ff6d48d5458a90cb12018ab4
behrooz pahlavan <pahlavan111@gmail.com>
2020-10-27 08:52:55 +0330

    add note view model

---
+++ app/src/main/java/com/bp/mvvmjava/viewmodel/NoteViewModel.java
@@ -0,0 +1,12 @@
+package com.bp.mvvmjava.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class NoteViewModel extends AndroidViewModel{
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
